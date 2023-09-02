a4cc0630b32931b7852c021312e330a4b7c7bc9c
Hariharan Thiagarajan <hariharansmm@gmail.com>
2023-09-02 16:46:21 +1200

    Second Commit
    Remove old Story Board
    Create new Controller for Home buttons - Home, Search, Upcoming, Downloads
    Set color

---
+++ Controllers/DownloadsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DownloadsViewController.swift
+//  OceanFlix
+//
+//  Created by Hariharan Thiagarajan on 2/09/23.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
