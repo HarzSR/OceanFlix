aca0534decefe0b2923b3b594ed7f8731afb761b
Hariharan Thiagarajan <hariharansmm@gmail.com>
2023-09-08 23:34:22 +1200

    Ninth Commit
    Create API file for TMDB

---
+++ Managers/APICaller.swift
@@ -0,0 +1,20 @@
+//
+//  APICaller.swift
+//  OceanFlix
+//
+//  Created by Hariharan Thiagarajan on 8/09/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let API_KEY = "cb51a0235318bb3385b2d2b4303e2584"
+    
+}
+
+class APICaller {
+    
+    static let shared = APICaller()
+    
+}
