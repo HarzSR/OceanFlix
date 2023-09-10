eeb29b6fd00d6f000381e426e4501ae154209263
Hariharan Thiagarajan <hariharansmm@gmail.com>
2023-09-11 00:17:08 +1200

    Twelfth Commit
    Fix API Calls - Able to see Output

--- Managers/APICaller.swift
+++ Managers/APICaller.swift
@@ -11,10 +11,7 @@ struct Constants {
     
     static let API_KEY = "cb51a0235318bb3385b2d2b4303e2584"
     static let baseURL = "https://api.themoviedb.org/3/"
-    static let headers = [
-        "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjUxYTAyMzUzMThiYjMzODViMmQyYjQzMDNlMjU4NCIsInN1YiI6IjY0ZmVmOWY4MmRmZmQ4MDBlM2QzNTJjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jw-yQF-w3rHhNOCgIotNBYtr9-SIfGe4aq_HXPcOhE4"
-      ]
+    static let bearer = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjUxYTAyMzUzMThiYjMzODViMmQyYjQzMDNlMjU4NCIsInN1YiI6IjY0ZmVmOWY4MmRmZmQ4MDBlM2QzNTJjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jw-yQF-w3rHhNOCgIotNBYtr9-SIfGe4aq_HXPcOhE4"
     
 }
 
@@ -23,21 +20,23 @@ class APICaller {
     static let shared = APICaller()
     
     func getTrendingMovies(completion: @escaping (String) -> Void) {
-        let request = NSMutableURLRequest(url: NSURL(string: "\(Constants.baseURL)trending/movie/day?language=en-US")! as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
-        request.httpMethod = "GET"
-        request.allHTTPHeaderFields = Constants.headers
-        
-        let session = URLSession.shared
-        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-            if (error != nil) {
-                print(error as Any)
+        guard let url = URL(string: "\(Constants.baseURL)trending/movie/day?language=en-US") else { return }
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue(Constants.bearer, forHTTPHeaderField: "Authorization")
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                return
             }
-            else {
-                let httpResponse = response as? HTTPURLResponse
-                print(httpResponse)
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
             }
-        })
-        task?.resume()
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
     }
     
 }
