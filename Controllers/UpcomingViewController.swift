ea90aa68ba92db7aa62f6378e8c1bb390d2fc28b
Hariharan Thiagarajan <hariharansmm@gmail.com>
2023-09-18 23:30:19 +1200

    Nineteenth Commit
    Update Upcoming Page

--- Controllers/UpcomingViewController.swift
+++ Controllers/UpcomingViewController.swift
@@ -8,15 +8,28 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    
+    private let upcomingTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
 
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .systemBackground
+        // view.backgroundColor = .systemBackground
     }
-    
 
     /*
     // MARK: - Navigation
@@ -28,4 +41,16 @@ class UpcomingViewController: UIViewController {
     }
     */
 
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
 }
