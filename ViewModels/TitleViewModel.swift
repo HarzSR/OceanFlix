fc85cbe19ac5efe5ccb2322ccbc79a844bd447de
Hariharan Thiagarajan <hariharansmm@gmail.com>
2023-09-20 02:50:36 +1200

    Twenty First Commit
    Update Upcoming UI
    Add Tables, Images, Names

---
+++ ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  OceanFlix
+//
+//  Created by Hariharan Thiagarajan on 20/09/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
---
+++ Views/TitleTableViewCell.swift
@@ -0,0 +1,83 @@
+//
+//  TitleTableViewCell.swift
+//  OceanFlix
+//
+//  Created by Hariharan Thiagarajan on 19/09/23.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    static let identifier = "TitleTableViewCell"
+    
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(systemName: "play.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 35))
+        button.setImage(image, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = UIColor.label
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 2
+        label.preferredMaxLayoutWidth = 225
+        return label
+    }()
+    
+    private let titlePosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titlePosterUIImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+        
+        applyConstrain()
+    }
+    
+    public func applyConstrain() {
+        let titlesPosterUIImageViewConstraints = [
+            titlePosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 105),
+        ]
+        let titleLabelConstraints = [
+            titleLabel.leadingAnchor.constraint(equalTo: titlePosterUIImageView.trailingAnchor, constant: 20),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        ]
+        let playTitleButtonConstraints = [
+            playTitleButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            playTitleButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(titlesPosterUIImageViewConstraints)
+        NSLayoutConstraint.activate(titleLabelConstraints)
+        NSLayoutConstraint.activate(playTitleButtonConstraints)
+    }
+    
+    public func configure (with model: TitleViewModel) {
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model.posterURL)") else {
+            return
+        }
+        
+        titlePosterUIImageView.sd_setImage(with: url, completed: nil)
+        titleLabel.text = model.titleName
+    }
+    
+    required init?(coder: NSCoder){
+        fatalError()
+    }
+
+}
