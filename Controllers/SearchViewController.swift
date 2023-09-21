650b88c61786733f3743630ec46f0c1e43f08ed4
Hariharan Thiagarajan <hariharansmm@gmail.com>
2023-09-21 23:39:20 +1200

    Twenty Second Commit
    Update Search Controller with Title and basic Layout

--- Controllers/SearchViewController.swift
+++ Controllers/SearchViewController.swift
@@ -8,15 +8,34 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private let discoverTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = UIColor.systemBackground
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(discoverTable)
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        // view.backgroundColor = .systemBackground
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
 
     /*
     // MARK: - Navigation
@@ -28,4 +47,18 @@ class SearchViewController: UIViewController {
     }
     */
 
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
 }
